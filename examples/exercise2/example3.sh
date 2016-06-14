ae17af64e44875b349d0368f027795eca9f5905e
7kn <tknoops@gmail.com>
2016-06-14 13:36:17 +0200

    editing typo's

--- examples/exercise2/example3.sh
+++ examples/exercise2/example3.sh
@@ -3,10 +3,10 @@ clear
 echo $0 started as $$
 echo "List the installed plugins"
 sudo bin/plugin list
-read -p "Press <RETURN> to install Kafa-Input Filter"
+read -p "Press <RETURN> to install Kafka-Input Filter"
 echo "First install the Kafka input filter"
 sudo bin/plugin install logstash-input-kafka
-read -p "Press <RETURN> to install Kafa-Output Filter"
+read -p "Press <RETURN> to install Kafka-Output Filter"
 echo "Now install Kafka output filter"
 sudo bin/plugin install logstash-output-kafka
 echo "Kafka plugins are now available"
